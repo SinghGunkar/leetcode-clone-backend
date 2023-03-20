719598f10e329afe0f6cf9056c2bb41e3d97a909
act18 <alvin_chun-yum_tsang@sfu.ca>
2023-03-20 11:51:53 -0700

    integrate login with db

--- frontend/pyIDE_frontEnd/src/app/services/server-api-at.service.ts
+++ frontend/pyIDE_frontEnd/src/app/services/server-api-at.service.ts
@@ -36,6 +36,18 @@ export class ServerApiATService {
     return this.http.post<any>(url, data);
   }
 
+    /**
+   * Check that the user exists in the db
+   * @param user        email of the user
+   * @param password    password for the user
+   * @returns           a string stating if the request was successful or if it failed
+   */
+    public login(user: string, password: string): Observable<any> {
+      let url: string = this.server_api + "login";
+      let data: any = {"user": user, 
+                        "password": password
+                      };
+      return this.http.post<any>(url, data);
+    }
 
-
-} // end of server-api-at.service.ts
+} // end of server-api-at.service.ts'
