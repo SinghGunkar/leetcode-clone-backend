2835e9e7c5402ead2f60051fb6ad506307324901
act18 <alvin_chun-yum_tsang@sfu.ca>
2023-03-21 23:45:37 -0700

    merge environment.ts

--- frontend/pyIDE_frontEnd/src/app/environment.ts
+++ frontend/pyIDE_frontEnd/src/app/environment.ts
@@ -1,5 +1,5 @@
 /**
- * This calss is usd to put some environment variables
+ * This class is usd to put some environment variables
  */ 
 export const environment = {
     backendEndpoint: 'http://127.0.0.1:8080/'
