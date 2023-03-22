73857e6f55c67543179502aa1309c8b5536a3bb2
act18 <alvin_chun-yum_tsang@sfu.ca>
2023-03-22 00:05:40 -0700

    move auth into auth service

--- frontend/pyIDE_frontEnd/src/app/auth.service.ts
+++ frontend/pyIDE_frontEnd/src/app/auth.service.ts
@@ -1,21 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './environment'; // import environment variables
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  logInStatus = false;
+  public logInStatus: boolean = false;
+  private server_api: string = environment.backendEndpoint;
 
   constructor(private http: HttpClient) { }
 
-  login(username: string, password: string) {
-     this.http.post('/api/login', { username, password }, 
-    { withCredentials: true }).subscribe((response)=>{
-      this.logInStatus = true;
-    },(error)=>{
-      this.logInStatus = false;
-      console.log(error);
-    });
+  /**
+   * Sign the new user up with the database
+   * @param user        email of the user
+   * @param password    password for the user
+   * @returns           a string stating if the request was successful or if it failed
+   */
+  public signup(user: string, password: string): Observable<any> {
+    let url: string = this.server_api + 'signup';
+    let data: any = {"user": user, "password": password};
+    return this.http.post<any>(url, data);
   }
-}
+
+  /**
+   * Check that the user exists in the db
+   * @param user        email of the user
+   * @param password    password for the user
+   * @returns           a string stating if the request was successful or if it failed
+   */
+  public login(user: string, password: string): Observable<any> {
+    let url: string = this.server_api + "login";
+    let data: any = {"user": user, "password": password};
+    return this.http.post<any>(url, data);
+  }
+
+} // end of auth.service.ts
