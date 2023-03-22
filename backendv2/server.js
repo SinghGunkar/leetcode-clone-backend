908479e8d3eb4a423204dfd5b9c1b08dcaef4163
GunkarSingh <gunkarsingh@hotmail.com>
2023-03-21 20:59:27 -0700

    create promise rejection handler

--- backendv2/server.js
+++ backendv2/server.js
@@ -13,3 +13,19 @@ const app = express()
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"))
 }
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+)
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
