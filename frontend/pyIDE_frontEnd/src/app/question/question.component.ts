c956bef5db8087face9d127053e583859d69338a
act18 <alvin_chun-yum_tsang@sfu.ca>
2023-04-02 16:26:38 -0700

    add question editor to admin role only

--- frontend/pyIDE_frontEnd/src/app/question/question.component.ts
+++ frontend/pyIDE_frontEnd/src/app/question/question.component.ts
@@ -2,6 +2,7 @@ import { QuestionServiceService } from './../question-service.service';
 import { Component, OnInit,ElementRef,ViewChild ,Renderer2} from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 
 @Component({
@@ -16,14 +17,23 @@ export class QuestionComponent {
   questionTitle: string;
   questionContent: string;
 
+  role: string;
+  isAdmin: boolean;
+
+
+
   // @ViewChild('questionContentBox') questionContentBox!: ElementRef;
   // questionContentBox = this.elem.nativeElement.querySelector('.questionContentBox');  
 
-  constructor(private elem: ElementRef,private router: Router, private route: ActivatedRoute, private questionService: QuestionServiceService,private renderer: Renderer2) {
+  constructor(private elem: ElementRef, private router: Router, 
+              private route: ActivatedRoute, private questionService: QuestionServiceService,
+              private renderer: Renderer2, private auth:AuthService) {
     this.id = -1;
     this.questionTitle = "";
     this.questionContent = "";
 
+    this.role = this.auth.getUserType();
+    this.isAdmin = this.role === "admin" ? true : false;
 
   }
 
